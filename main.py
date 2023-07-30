f33df0ce620c1a681b74195456482d9206e515ae
OlehUkhanov <kompanitaptutraj@gmail.com>
2023-07-30 19:06:06 +0300

    Ready to test

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+from sentence_transformers import SentenceTransformer, util
 import openai
 import asyncio
-import re
 import whisper
-import boto3
 import pydub
 from pydub import playback
 import speech_recognition as sr
-from EdgeGPT import Chatbot, ConversationStyle
-from numba import jit
 import json
 import os
+import torch
 
 # Initialize the OpenAI API
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -17,40 +15,58 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Create a recognizer object and wake word variables
 recognizer = sr.Recognizer()
 
-def synthesize_speech(text, output_filename):
-  polly = boto3.client('polly', region_name='us-west-2')
-  response = polly.synthesize_speech(
-    Text=text,
-    OutputFormat='mp3',
-    VoiceId='Salli',
-    Engine='neural'
-  )
-
-  with open(output_filename, 'wb') as f:
-    f.write(response['AudioStream'].read())
-
 def play_audio(file):
   sound = pydub.AudioSegment.from_file(file, format="mp3")
   playback.play(sound)
 
 async def main():
+  embedder = SentenceTransformer('all-MiniLM-L6-v2')
+
   try:
     with open("./database.json", 'r') as f:
-        contents = json.load(f)
-        print(contents.keys())
+      contents = json.load(f)
+      corpus = list(contents.keys())
+      corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
   except Exception as e:
     print(e)
 
-  """
-  try:
-    model = whisper.load_model("base.en")
-    result = model.transcribe("audio.wav", fp16=False)
-    phrase = result["text"]
-    print(f"You said: {phrase}")
-    
-  except Exception as e:
-    print("Error transcribing audio: {0}".format(e))
-  """
+  while True:
+    with sr.Microphone() as source:
+      recognizer.adjust_for_ambient_noise(source)
+      print(f"----- Waiting for new question -----\n")
+      audio = recognizer.listen(source)
+      try:
+        with open("audio.wav", "wb") as f:
+          f.write(audio.get_wav_data())
+        # Use the preloaded tiny_model
+        model = whisper.load_model("tiny")
+        result = model.transcribe("audio.wav", fp16=False)
+        question = result["text"]
+        print("You asked : ", question, "\n")
+
+        if question == "I will quit":
+          break
+
+        queries = [question]
+        top_k = min(1, len(corpus))
+        for query in queries:
+          query_embedding = embedder.encode(query, convert_to_tensor=True)
+        
+        cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
+        top_results = torch.topk(cos_scores, k=top_k)
+
+        top_score = -1
+        top_score_question = ''
+        for score, idx in zip(top_results[0], top_results[1]):
+          if top_score < score:
+            top_score_question = corpus[idx]
+        print(corpus[idx], " -> audio : ", contents[top_score_question], "(Score: {:.4f})".format(score), "\n")
+
+        play_audio('audio/' + contents[top_score_question])
+        print("\n\n")
+      except Exception as e:
+        print("Error : {0}".format(e))
+        continue
 
 if __name__ == "__main__":
   asyncio.run(main())
